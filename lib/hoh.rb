069b1a4ed4b4863636aa3d8665772a939875a864
Jordan Melidor-Fuxis <jordan.melidor@hotmail.fr>
2020-03-23 17:36:55 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -46,11 +46,14 @@ def naming_system
    sub_category: {
      label: "Phylum",
      sub_category: {
-       label: "Class"{
-         label: "Order"{
+       label: "Class",
+       sub_category: {
+         label: "Order"
+         sub_category: {
            label: "Family",
               sub_category: {
-                label: "Genus"{
+                label: "Genus"
+                sub_category: {
                   label: "Species",
                     sub_category: nil
                 }
